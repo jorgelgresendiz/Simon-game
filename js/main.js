909e8a52bf95c275e7ad93aafd9336d8384034c5
Jorge Galvan <jorgegalvan@Jorges-MacBook-Air.local>
2019-10-31 13:48:54 -0500

    begin adding pseudocode for in JS

--- js/main.js
+++ js/main.js
@@ -1,8 +1,35 @@
 //constants
 
+let audio = {
+    box1: new Audio (),
+    box2: new Audio (),
+    box3: new Audio (),
+    box4: new Audio ()
+};
+
 //state variables
+
+var originalTable = [];
+var playerTable = [];
 
 //cached elements
 
+
+
 //functions
 
+function nextLevel(){
+
+}
+
+function playSounds(){
+
+}
+
+function clickEvent(){
+
+}
+
+function checkGuesses(){
+    
+}
