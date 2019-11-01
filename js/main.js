30002a1d5c3718bca79d917278a398c48ee7a40a
Jorge Galvan <jorgegalvan@Jorges-MacBook-Air.local>
2019-10-31 20:29:38 -0500

    add audio source to html and write event listener

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 //constants
 
-let audio = {
-    box1: new Audio (),
-    box2: new Audio (),
-    box3: new Audio (),
-    box4: new Audio ()
-};
+// let audio = {
+//     box1: new Audio (),
+//     box2: new Audio (),
+//     box3: new Audio (),
+//     box4: new Audio ()
+// };
 
 //state variables
 
@@ -14,10 +14,25 @@ var playerTable = [];
 
 //cached elements
 
+const box1 = document.getElementById("box1");
+const box2 = document.getElementById("box2");
+const box3 = document.getElementById("box3");
+const box4 = document.getElementById("box4");
+
+
+
+//eventlisteners
+
+box1.addEventListener('click', evt);
+
+box2.addEventListener('click', evt);
+
+box3.addEventListener('click',evt);
+
+box4.addEventListener('click',evt);
 
 
 //functions
-
 function nextLevel(){
 
 }
@@ -31,5 +46,5 @@ function clickEvent(){
 }
 
 function checkGuesses(){
-    
+
 }
