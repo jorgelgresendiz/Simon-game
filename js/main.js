44c8e05924498a18661f69e844ea9ff7ed00cf61
Jorge Galvan <jorgegalvan@Jorges-MacBook-Air.local>
2019-11-04 11:52:09 -0600

    create for loop within start game function that randomly creates and pushes numbers 1-4 into original table array

--- js/main.js
+++ js/main.js
@@ -1,45 +1,61 @@
 //constants
-
+let win;
+let sound;
 
 //state variables
 
-var originalTable = [];
+let originalTable = [];
 var playerTable = [];
 
 //cached elements
 
-const box1 = document.getElementById("box1");
-const box2 = document.getElementById("box2");
-const box3 = document.getElementById("box3");
-const box4 = document.getElementById("box4");
-const startButton = document.getElementById("start");
+const startBtn = document.getElementById("start");
 
+const box1  = document.getElementById("box1");
+const box2  = document.getElementById("box2");
+const box3  = document.getElementById("box3");
+const box4  = document.getElementById("box4");
+
+const levels = document.getElementById("levels");
 
 
 //eventlisteners
 
-box1.addEventListener('click', evt);
+//event listeners for e/ color 
 
-box2.addEventListener('click', evt);
+box1.addEventListener('click', (event) => {
+    console.log("working")
+})
+box2.addEventListener('click', (event) => {
+    console.log("working")
+})
+box3.addEventListener('click', (event) => {
+    console.log("working")
+})
+box4.addEventListener('click', (event) => {
+    console.log("working")
+})
 
-box3.addEventListener('click',evt);
-
-box4.addEventListener('click',evt);
-
+//add evt listener to btn and run cb function to trigger beginning of game
+startBtn.addEventListener('click', startGame);
 
 //functions
-function nextLevel(){
 
+function startGame(){
+    originalTable = [];
+    playerTable = [];
+    levels.innerHTML = "Level 1";
+//create a random number between 1 and 4 and push into array original table
+    for (let i = 0; i < 20; i++){
+        originalTable.push(Math.ceil(Math.random() * 4));
+    }
+    console.log(originalTable);
 }
+ 
+function simonTurn(){
+//match the series of random numbers generated and play their corresponding song.
 
-function playSounds(){
+};
 
-}
 
-function clickEvent(){
 
-}
-
-function checkGuesses(){
-
-}
