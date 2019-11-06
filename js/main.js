93b453d8b470abbaa50bd6808f35435f9268e36e
Jorge Galvan <jorgegalvan@Jorges-MacBook-Air.local>
2019-11-06 16:15:04 -0600

    buttons have assigned numbers and brighten when randomly selected

--- js/main.js
+++ js/main.js
@@ -1,16 +1,13 @@
 //constants
-const numLevels = 20;
 var level = 0;
 
 //state variables
-
-let originalTable = [];
-let playerTable = [];
+var gameTable = [];
+var playerTable = [];
 
 //cached elements
-
 const startBtn = document.getElementById("start");
-
+const restartBtn = document.getElementById("restart");
 const box1  = document.getElementById("box1");
 const box2  = document.getElementById("box2");
 const box3  = document.getElementById("box3");
@@ -20,76 +17,95 @@ const levels = document.getElementById("levels");
 
 
 //eventlisteners
-
-//event listeners for e/ color 
-
 box1.addEventListener('click', (event) => {
-
+    playerTable.push(event.target.id);
+    //push clicking to user array
+    console.log(playerTable);
 })
 
-
 box2.addEventListener('click', (event) => {
+    playerTable.push(event.target.id);
+    console.log(playerTable);
 })
 
 box3.addEventListener('click', (event) => {
+    playerTable.push(event.target.id);
+    console.log(playerTable);
 })
 
 box4.addEventListener('click', (event) => {
+    playerTable.push(event.target.id);
+    console.log(playerTable);
 })
 
-//add evt listener to btn and run cb function to trigger beginning of game
+//add evt listener to start btn and run cb function to trigger beginning of game
 startBtn.addEventListener('click', startGame);
+
+//add evt listener to restart btn and clear all code out
+restartBtn.addEventListener('click', restartGame);
 
 //functions
 
-function startGame(){
-    originalTable = [];
-    playerTable = [];
-    levels.innerHTML = "Level 1, good luck!";
-//runs simonTurn function after 1 sec    
-    setInterval(simonTurn, 1000);
-}
- 
-//random number generated in a function and pushed into array
+//create a random number between 1 and 4 and push into array original table
 function randomNumber(){
-    originalTable.push(Math.ceil(Math.random() * 4));
-}
-
-//simon function will display the current level
-function simonTurn(){
+    var randomNumber = (Math.ceil(Math.random() * 4));
+    gameTable.push(randomNumber);
+    //immediately brightens button after respective random number found
     brightenButton();
+};
+
+//trigerred when start button is clicked
+function startGame(){
+    randomNumber();
+    playerTable = [];
+    levels.innerHTML = "Level 1";    
+    setInterval(simonTurn, 1000);
+    console.log(gameTable);
+};
+
+
+// function changes the preset opaque colors to bright colors when found in gametable array
+function brightenButton() {
+    gameTable.forEach(i => {
+        if (i === 1) {
+             box1.style.backgroundColor = "green";
+         } if (i === 2) {
+            box2.style.backgroundColor = "red";
+         } if (i === 3) {
+             box3.style.backgroundColor = "yellow";
+        } if (i === 4) {
+             box4.style.backgroundColor = "blue";
+         }
+    })
+};
+
+
+//simon function
+function simonTurn(){
 
 };
 
-function boxOne(){
-    let audio = document.getElementById("soundBox1");
-    Audio.play();
+
+function restartGame(){
+    randomTable = [];
+    originalTable = [];
 };
 
-function boxTwo(){
-    let audio = document.getElementById("soundBox1");
-    Audio.play();
-};
 
-function boxThree(){
-    let audio = document.getElementById("soundBox1");
-    Audio.play();
-};
 
-function boxFour(){
-    let audio = document.getElementById("soundBox1");
-    Audio.play();
-};
 
-function checkWin(){
+// function checkWin(){
+//     gameTable.forEach(function(i){
 
-}
+//     if (JSON.stringify(originalTable) === JSON.stringify(playerTable)){
+//         document.getElementById("display").innerText = "Congratulations, you made it to the next level!";
+//     } else {
+//         document.getElementById("display").innerText = "Sorry, try again!";
+//     }
+// });
 
-//function changes the preset opaque colors to bright colors
-function brightenButton(){
-    box1.style.backgroundColor("green");
-    box2.style.backgroundColor("red");
-    box3.style.backgroundColor("yellow");
-    box4.style.backgroundColor("blue");
-};
+
+
+
+
 
